1b6ab425bd4543de7fdb106e48b6fc6e6481f1ee
Olanike Akhigbe <olanike.akhigbe@accenture.com>
2026-07-08 22:32:36 +0100

    Add project files.

---
+++ EmployeeDirectory/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeDirectory.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeDirectory.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var employees = new List<Employee>
+            {
+                new Employee { Id = 1001, Name = "Alice Johnson", Department = "Human Resources" },
+                new Employee { Id = 1002, Name = "James Smith", Department = "Finance" },
+                new Employee { Id = 1003, Name = "Sarah Williams", Department = "IT Support" }
+            };
+
+            return View(employees);
+        }
+    }
+}
